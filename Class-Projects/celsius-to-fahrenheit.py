6e29f098e0e302797f56fc889432e7f881b03a9c
Jit Mukherjee <133637574+GTAJIT@users.noreply.github.com>
2024-10-15 12:38:18 +0530

    Create celsius-to-fahrenheit.py

---
+++ Class-Projects/celsius-to-fahrenheit.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+# Example Usage:
+celsius = 25
+print(f"{celsius}°C is equal to {celsius_to_fahrenheit(celsius)}°F")
