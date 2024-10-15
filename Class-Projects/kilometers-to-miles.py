e722bb4cc8ed75d6327bb89a8864d061cdc7cc13
Jit Mukherjee <133637574+GTAJIT@users.noreply.github.com>
2024-10-15 12:38:26 +0530

    Create kilometers-to-miles.py

---
+++ Class-Projects/kilometers-to-miles.py
@@ -0,0 +1,7 @@
+def kilometers_to_miles(kilometers):
+    miles = kilometers * 0.621371
+    return miles
+
+# Example Usage:
+kilometers = 10
+print(f"{kilometers} km is equal to {kilometers_to_miles(kilometers)} miles")
